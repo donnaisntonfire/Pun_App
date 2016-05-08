647b07a8726b30ad01f4f00d3b47bafab8f39fe5
donnaisntonfire <lai.donna@outlook.com>
2016-05-08 13:40:09 -0700

    updated puns

--- app/src/main/java/fionaanddonna/punapp2/MainActivity.java
+++ app/src/main/java/fionaanddonna/punapp2/MainActivity.java
@@ -16,11 +16,30 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[] PunsArray = new String[]{"How many tickles does it take for a squid to laugh? Ten tickles",
-            "Which president was least guilty Lincoln. He was in a cent.",
-            "A photon checks into a hotel and is asked if he needs help with luggage. No, i’m traveling light.”",
-            "What do you call an alligator in a vest? An investigator.", "What do you call a thieving alligator? A crookodile. ",
-            "What do you call a cow with no legs? Ground beef.", "What do you call a pig that does karate? A pork chop."};
+    String[] PunsArray = new String[]
+            {"How many tickles does it take for a squid to laugh? Ten tickles",
+                    "Which president was least guilty Lincoln. He was in a cent.",
+                    "A photon checks into a hotel and is asked if he needs help with luggage. No, i’m traveling light.”",
+                    "What do you call an alligator in a vest? An investigator.",
+                    "What do you call a thieving alligator? A crookodile. ",
+                    "What do you call a cow with no legs? Ground beef.", "What do you call a pig that does karate? A pork chop.",
+                    "What did the buffalo say to his son when he left for college? Bison.", "Wanna hear a joke about paper? Nevermind, it’s tearable.",
+                    "What do you call a fake noodle? An impasta.", "Why couldn’t the bike stand on its own? It’s two tired.",
+                    "Why couldn’t the bike stand on its own? It’s two tired.","How do you organize a space party? You planet.",
+                    "How do you put a baby alien to sleep? You rocket.","What job did the frog have at the hotel? Bellhop.",
+                    "Why did the banana go to the doctor? He wasn’t peeling well.",
+                    "What has eyes but cannot see? A potato.",
+                    "Two blood cells met and fell in love. Alas, it was all in vein.",
+                    "Never trust an atom. They make everything up.",
+                    "What happens to light when they break the law? In prism.",
+                    "I told a chemistry joke. There was no reaction.",
+                    "What kind of bee can’t make up its mind? A may be.",
+                    "What did the hamburger name its baby? Patty.",
+                    "What do you get from a pampered cow? Spoiled milk.",
+                    "What does a volcano do when it wants food? It orders a cratering company.",
+
+
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -49,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -57,7 +77,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
