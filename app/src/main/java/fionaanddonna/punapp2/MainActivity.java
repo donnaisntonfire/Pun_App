64005f16e4a7deedb69c55b49e50d39256e1de04
donnaisntonfire <lai.donna@outlook.com>
2016-05-11 16:01:06 -0700

    full list of puns title change

--- app/src/main/java/fionaanddonna/punapp2/MainActivity.java
+++ app/src/main/java/fionaanddonna/punapp2/MainActivity.java
@@ -37,8 +37,31 @@ public class MainActivity extends AppCompatActivity {
                     "What did the hamburger name its baby? Patty.",
                     "What do you get from a pampered cow? Spoiled milk.",
                     "What does a volcano do when it wants food? It orders a cratering company.",
-
-
+                    "Why did the three little pigs fall asleep every time grandpa told a story? Because grandpa was a boar.",
+                    "What do you call a fish with no eye? A fsh",
+                    "What’s a pirate’s favorite subject? Arrrrrt.",
+                    "What do you call a sad strawberry? A blueberry.",
+                    "Why wasn’t the teddy bear hungry? Because he was stuffed.",
+                    "What do you call a dog owned by dracula? A bloodhound. ",
+                    "What do you call an owl that does magic tricks? Hoodini.",
+                    "What do you call a bear with no teeth? A gummy bear.",
+                    "I read my friend a list of ten puns to make her laugh. Not one pun in ten did.",
+                    "Bird puns just fly over my head.",
+                    "A friend of mine tried to annoy me with bird puns, but i soon realized that toucan play at that game.",
+                    "What kind of bird sticks to a sweater? A vel-crow.",
+                    "What does a noesy pepper do? Gets jalapeno business. ",
+                    "What do you call a snobbish criminal going down the stairs? A condescending con descending. ",
+                    "What kind of shorts do clouds wear? Thunderwear.",
+                    "Becoming a vegetarian is a big missed steak.",
+                    "What do you call a lazy kangaroo? A pouch potato.",
+                    "What do you get when dinosaurs crash their cars?  T-Rex.",
+                    "What do you call an everyday potato? A commentator.",
+                    "I hate insect puns. They really bug me.",
+                    "What do you call a pile of cats? A meowtain. ",
+                    "Yesterday a book fell on my head. I only have my shelf to blame.",
+                    "What do you call a dinosaur with an extensive vocabulary? A thesaurus.",
+                    "Why are frogs so happy? They eat whatever bugs them.",
+                    "What did one ocean say to the other? Nothing, they just waved. Sea what I did there? I’m shore you did."
             };
 
     @Override
@@ -46,8 +69,7 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         Button puns = (Button) findViewById(R.id.button);
         puns.setOnClickListener(new ButtonListener());
@@ -61,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
             TextView textView2 = (TextView) findViewById(R.id.textView2);
             Random random = new Random();
             int index = random.nextInt(PunsArray.length);
-
             textView2.setText(PunsArray[index]);
 
             //System.out.println(PunsArray[index]);
